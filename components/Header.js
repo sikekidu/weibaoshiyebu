ed585ae12c270b789ebe5c9728f31be80832542f
silva <silva@vbox.myguest.virtualbox.org>
2024-11-01 15:28:18 +0800

    超链接悬停变色

--- components/Header.js
+++ components/Header.js
@@ -3,22 +3,40 @@ import Link from 'next/link';
 import Image from 'next/image';
 import styles from '../styles/Header.module.css';
 import logoImage from '../public/shsdb.png';
+import headerBackground from '../public/747374890227005468.jpg'; // 引入背景图片
 
 const Header = () => (
-  <header className={styles.header} style={{ marginTop: '0px' }}>
+  <header 
+    className={styles.header} 
+    style={{ 
+      backgroundImage: `url(${headerBackground.src})`, // 设置背景图片
+      backgroundSize: 'cover', // 确保背景图片完全覆盖
+      backgroundPosition: 'center', // 图片居中显示
+      height: '1000px', // 设置头部高度，你可以根据需要调整
+      marginTop: '0px'
+    }}
+  >
     <div className={styles.leftSection} style={{ alignItems: 'flex-start' }}>
-      <Link href="/" legacyBehavior>
-        <a>
+      <Link href="https://www.stec.sh.cn/#/home" legacyBehavior>
+        <a style={{ cursor: 'pointer' }}> {/* 添加鼠标手型的超链接标志 */}
           <Image src={logoImage} alt="上海隧道 Logo" className={styles.logoImage} />
         </a>
       </Link>
-      <h1 className={styles.logo} style={{ marginTop: '45px', marginLeft: '-145px' }}>设备维保事业部</h1>
+      <Link href="/">
+        <h1 className={styles.logo} style={{ marginTop: '45px', marginLeft: '-145px', cursor: 'pointer' }}>设备维保事业部</h1>
+      </Link>
     </div>
     <nav className={styles.rightSection} style={{ marginTop: '45px' }}>
       <ul className={styles.navList} style={{ alignItems: 'flex-end' }}>
-        <li className={styles.navItem}><Link href="#slurry-system">泥水系统</Link></li>
-        <li className={styles.navItem}><Link href="#shield-support">盾构施工配套</Link></li>
-        <li className={styles.navItem}><Link href="#shield-guard">盾构机保驾</Link></li>
+        <li className={styles.navItem}>
+          <Link href="#slurry-system" className={styles.navLink}>泥水系统</Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href="#shield-support" className={styles.navLink}>盾构施工配套</Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href="#shield-guard" className={styles.navLink}>盾构机保驾</Link>
+        </li>
       </ul>
     </nav>
   </header>
